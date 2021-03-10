dd98d75df98d9677a4ca9bc2ea6c4b6057e681d1
Perry E <its_Q@hotmail.com>
2021-03-10 23:24:59 +0100

    Add project files.

---
+++ SudokuSolver/Program.cs
@@ -0,0 +1,95 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace SudokuSolver
+{
+    class Program
+    {
+        public static int[] input = new int[] 
+        { 
+            3,0,1,9,0,8,0,0,5,
+            5,0,0,0,0,3,0,0,0,
+            0,6,0,0,5,0,0,8,0,
+            1,9,6,0,0,0,0,0,0,
+            0,2,3,0,0,0,4,7,0,
+            0,0,0,0,0,0,2,9,1,
+            0,3,0,0,4,0,0,1,0,
+            0,0,4,7,0,0,0,0,8,
+            6,0,5,3,0,1,9,0,0
+        };
+
+        public static int[] inputSolved = new int[]
+{
+            3,4,1,9,6,8,7,2,5,
+            5,8,9,2,7,3,1,6,4,
+            7,6,2,1,5,4,3,8,9,
+            1,9,6,4,2,7,8,5,3,
+            8,2,3,5,1,9,4,7,0,
+            4,5,7,8,3,6,2,9,1,
+            9,3,8,6,4,2,5,1,7,
+            2,1,4,7,9,5,6,3,8,
+            6,7,5,3,8,1,9,4,2
+};
+
+        public static List<SLine> sLines = new List<SLine>();
+
+        static void Main(string[] args)
+        {
+
+            int currentInputIndex = 0;
+            Random r = new Random();
+
+            Console.WriteLine("Loading solver...");
+            Solver s = new Solver();
+            var listOfPer = SolverHelper.Permute(new int[] { 1,2,3,4,5,6,7,8,9 });
+            Console.WriteLine($"Possible line solutions: {listOfPer.Count}");
+            Console.WriteLine($"Possible line length: {listOfPer[0].Count}");
+            Console.WriteLine($"Check count at {1}: {listOfPer.Where(x => x[0] == 1).Count()}");
+
+            Console.WriteLine("Calculating possible permutations...");
+            Console.WriteLine("------------------------------");
+            for (int l = 0; l < 9; l++)
+            {
+                SLine sl = new SLine();
+                sl.data = input.GetNext(l, 0);
+                sl.possiblePermutations = sl.data.GetPermutations(listOfPer);
+                sLines.Add(sl);
+
+                Console.WriteLine($"Line {l+1} has {sl.possiblePermutations.Count} possible solutions");
+            }
+            Console.WriteLine("------------------------------");
+
+            for (int l = 0; l < 9; l++)
+            {
+                SLine sl = new SLine();
+                sl.data = input.GetNext(l, 1);
+                sl.possiblePermutations = sl.data.GetPermutations(listOfPer);
+                sLines.Add(sl);
+
+                Console.WriteLine($"Line {l + 1} has {sl.possiblePermutations.Count} possible solutions");
+            }
+            Console.WriteLine("------------------------------");
+
+            for (int l = 0; l < 9; l++)
+            {
+                SLine sl = new SLine();
+                sl.data = input.GetNext(l, 2);
+                sl.possiblePermutations = sl.data.GetPermutations(listOfPer);
+                sLines.Add(sl);
+
+                Console.WriteLine($"Line {l + 1} has {sl.possiblePermutations.Count} possible solutions");
+            }
+            Console.WriteLine("------------------------------");
+
+            Console.WriteLine("permutations calculated.");
+            Console.WriteLine("Calculating possible combos...");
+
+            
+
+            string solvedStr = s.Solve(inputSolved) ? "yes" : "no";
+            Console.WriteLine($"Solved: {solvedStr}");
+            Console.ReadLine();
+        }
+    }
+}
---
+++ SudokuSolver/Solver.cs
@@ -0,0 +1,154 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Linq;
+using System.Collections;
+
+namespace SudokuSolver
+{
+    public class SLine
+    {
+        public List<int> data;
+        public List<List<int>> possiblePermutations;
+    }
+        
+
+    public static class SolverHelper
+    {
+        public static string IntToString(this List<int> data)
+        {
+            string returnValue = "";
+            for (int i = 0; i < data.Count; i++)
+            {
+                returnValue += data[i].ToString() + " ";
+            }
+            return returnValue;
+        }
+
+        public static List<int> GetNext(this int[] data, int startingIndex, int type)
+        {
+            List<int> result = new List<int>();
+            if (type == 0)
+            {
+                for (int i = startingIndex*9; i < (startingIndex * 9) +9; i++)
+                {
+                    result.Add(data[i]);
+                }
+                return result;
+            }
+            if (type == 1)
+            {
+                for (int i = startingIndex; i < 81; i+=9)
+                {
+                    result.Add(data[i]);
+                }
+                return result;
+            }
+            if (type == 2)
+            {
+                int correctedIndex = 0;
+
+
+                for (int t = 0; t < 27; t+=9)
+                {
+                    for (int i = 0; i < 3; i++)
+                    {
+                        if (startingIndex == 0)
+                            correctedIndex = 0;
+                        if (startingIndex == 1)
+                            correctedIndex = 3;
+                        if (startingIndex == 2)
+                            correctedIndex = 6;
+                        if (startingIndex == 3)
+                            correctedIndex = 27;
+                        if (startingIndex == 4)
+                            correctedIndex = 30;
+                        if (startingIndex == 5)
+                            correctedIndex = 33;
+                        if (startingIndex == 6)
+                            correctedIndex = 54;
+                        if (startingIndex == 7)
+                            correctedIndex = 57;
+                        if (startingIndex == 8)
+                            correctedIndex = 60;
+                        int s = correctedIndex + t + i;
+
+                        result.Add(data[s]);
+                    }
+                }
+                return result;
+            }
+            return null;
+        }
+
+        public static List<List<int>> GetPermutations(this List<int> data, List<List<int>> source)
+        {
+            List<List<int>> result = null;
+            for (int i = 0; i < 9; i++)
+            {
+                if (data[i] == 0)
+                    continue;
+                if (result == null)
+                    result = source.Where(x => x[i] == data[i]).ToList();
+                else
+                    result = result.Where(x => x[i] == data[i]).ToList();
+            }
+            if (result == null)
+                return source;
+            return result;
+        }
+
+        public static List<List<int>> Permute(int[] nums)
+        {
+            var list = new List<List<int>>();
+            return DoPermute(nums, 0, nums.Length - 1, list);
+        }
+
+        static List<List<int>> DoPermute(int[] nums, int start, int end, List<List<int>> list)
+        {
+            if (start == end)
+            {
+                // We have one of our possible n! solutions,
+                // add it to the list.
+                list.Add(new List<int>(nums));
+            }
+            else
+            {
+                for (var i = start; i <= end; i++)
+                {
+                    Swap(ref nums[start], ref nums[i]);
+                    DoPermute(nums, start + 1, end, list);
+                    Swap(ref nums[start], ref nums[i]);
+                }
+            }
+
+            return list;
+        }
+
+        static void Swap(ref int a, ref int b)
+        {
+            var temp = a;
+            a = b;
+            b = temp;
+        }
+    }
+
+    class Solver
+    {
+        public bool Solve(int[] data)
+        {
+            if (!IsValidLine(data)) 
+                return false;
+            return true;
+        }
+
+        public bool IsValidLine(int[] data)
+        {
+            if (data.Distinct().Count() != 9)
+                return false;
+            return true;
+        }
+
+
+    }
+}
